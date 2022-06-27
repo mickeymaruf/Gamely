031fe2d8bde691a6fd1d2bbdb52459f2f3d23f28
mickeymaruf <thegoldenpopo@gmail.com>
2022-06-27 15:29:08 +0600

    cart and cat front

--- Gamely/views.py
+++ Gamely/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from store.models import Category, Product
@@ -5,8 +6,12 @@ from store.models import Category, Product
 # Create your views here.
 
 def home(request):
-    products = Product.objects.filter(stock__gt=0, is_available=True).order_by("-created")
-    categories = Category.objects.all()
+    products = Product.objects.filter(stock__gt=0, is_available=True).order_by("-created")[:12]
+    categories = Category.objects.all()[:8]
     
     context = {'products': products, 'categories': categories}
     return render(request, 'index.html', context)
+
+def category(request):
+    context = {}
+    return render(request, 'store/category.html', context)
